419673e40eb9664bdec0fbd1ac5ad6b923bc9449
Nguyen Anh Minh <anhminh.nguyen@ace.tamut.edu>
2025-05-21 21:13:44 -0500

    Update togetherjs-min.js

--- docs/togetherjs-min.js
+++ docs/togetherjs-min.js
@@ -543,11 +543,7 @@
     return "TogetherJS";
   };
 
-  var defaultHubBase = "https://hub.togetherjs.com";
-  if (defaultHubBase == "__" + "hubUrl"+ "__") {
-    // Substitution wasn't made
-    defaultHubBase = "https://hub.togetherjs.mozillalabs.com";
-  }
+  var defaultHubBase = "https://zedttxj.github.io/togetherjs/togetherjs-min.js";
   defaultConfiguration.hubBase = defaultHubBase;
 
   TogetherJS._configuration = {};
